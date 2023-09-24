f4d2162138abf40e445340c2a9e96784a5220929
marijana82 <marijana.mikolcic@gmail.com>
2023-09-24 22:01:34 +0200

    created directories controllers, dtos, exceptions, models, repositories and services

---
+++ src/main/java/com/relations/les13relations/Les13relationsApplication.java
@@ -0,0 +1,13 @@
+package com.relations.les13relations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les13relationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Les13relationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/relations/les13relations/Les13relationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relations.les13relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les13relationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
